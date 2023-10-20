36cb2d5c2aafa0e4d802cbc843e48b4322e82d37
officialsurajraj <surajraj41187@gmail.com>
2023-10-20 08:46:26 +0530

    commant out the all files

--- conversion_1.js
+++ conversion_1.js
@@ -1,57 +1,52 @@
-// let score = 10;
+let score = 10;
 let score1 = "10as";
 
-// console.log(typeof score);
-// console.log(typeof(score));
-// console.log(typeof score1);
-console.log(typeof Number(score1));
+ console.log(typeof score);
+ console.log(typeof(score));
+ console.log(typeof score1);
+ console.log(typeof Number(score1));
 
-// const value = Number(score1);
-// console.log(typeof value);
-// console.log(value)
-
-
-
-// const hello = "Google.com";
-// console.log(typeof(hello));
-// console.log( typeof Number (hello));
-// console.log(typeof hello);
-
-// const change = Number(hello);
-// console.log(change);
+ const value = Number(score1);
+ console.log(typeof value);
+ console.log(value)
 
 
-// let Google ;
-// let Empty = null;
 
-// console.log(Google);
-// console.log(Empty);
+ const hello = "Google.com";
+ console.log(typeof(hello));
+ console.log( typeof Number (hello));
+ console.log(typeof hello);
+ const change = Number(hello);
+ console.log(change);
 
 
-// console.log( Number (Google));
-// console.log(typeof Number(Google));
+let Google ;
+let Empty;
+console.log(Google);
+console.log(Empty);
 
-// const UserName = "officialsurajraj";
-// const password = "Hacker@111";
-// const otherPass = "12345";
-// const personalPass = 9770;
-// const marriged = true;
 
-// console.log(UserName);
-// console.log(typeof UserName);
-// console.log(typeof Number(UserName));
-// console.log(typeof UserName);
+console.log( Number (Google));
+console.log(typeof Number(Google));
+const UserName = "officialsurajraj";
+const password = "Hacker@111";
+const otherPass = "12345";
+const personalPass = 9770;
+const marriged = true;
 
-// const convert = Number(UserName);
-// console.log( typeof convert);
-// console.log( typeof Number (marriged));
-// console.log( typeof marriged)
+ console.log(UserName);
+ console.log(typeof UserName);
+ console.log(typeof Number(UserName));
+ console.log(typeof UserName);
+ const convert = Number(UserName);
+ console.log( typeof convert);
+ console.log( typeof Number (marriged));
+ console.log( typeof marriged)
 
-// const marrige = String (marriged);
-// console.log( typeof marrige);
-
-// const save = Number(prompt("this is Number value"));
-// console.log(save);
+ const marrige = String (marriged);
+ console.log( typeof marrige);
+ const save = Number(prompt("this is Number value"));
+ console.log(save);
 
 
 
