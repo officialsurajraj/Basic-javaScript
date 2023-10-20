4f7323b298a11809f95730cd89ce3decdb6ab0a5
officialsurajraj <surajraj41187@gmail.com>
2023-10-20 09:24:49 +0530

    change

--- Array_14.js
+++ Array_14.js
@@ -13,7 +13,7 @@ console.log(array.length);
 
 // Note: Indexing of array always starts from 0
 
-console.log(arry[0]);
+console.log(array[0]);
 //output:1
 
 console.log(array[1]);
@@ -41,31 +41,28 @@ console.log(Myheroes[4])
 
 const studentName = ["Suraj Raj","Ankit","Rohit","abhishek","Raj"];
 console.log(studentName);
+// output: ['Suraj Raj','Ankit','Rohit','abhishek','Raj'];
 
 studentName.push("ajay","vijay","kartik");
 console.log(studentName)
-
+// output: ['Suraj Raj','Ankit','Rohit','abhishek','Raj','ajay','vijay','kartik'];
 
 studentName.pop();
 console.log(studentName);
+// output: ['Suraj Raj','Ankit','Rohit','abhishek','Raj','ajay','vijay'];
 
 studentName.unshift("Hacker","Hacking","CyberSecurity");
 console.log(studentName);
+// output: ['Hacker','Hacking','CyberSecurity','Suraj Raj','Ankit','Rohit','abhishek','Raj','ajay','vijay'];
+
 
 studentName.shift()
 console.log(studentName)
+// output: ['Hacking','CyberSecurity','Suraj Raj','Ankit','Rohit','abhishek','Raj','ajay','vijay'];
 
 studentName.shift()
 console.log(studentName)
-
-
-output:
-[
-  'Suraj Raj', 'Ankit',   
-  'Rohit',     'abhishek',
-  'Raj',       'ajay',    
-  'vijay',     'kartik'   
-]
+// output: ['CyberSecurity','Suraj Raj','Ankit','Rohit','abhishek','Raj','ajay','vijay'];
 
 
 // Note: push() ==> push function we are used for add element backside in array
@@ -77,6 +74,8 @@ output:
 
 
 // includes():methods
+// Note : indcludes() : methods ka use hum apni puri array se yah pta lagane ke liye karte hai jo hum value de rahe hai bo array mein 
+// present hai ya nhi agar value present hogi to hume output true milenega otherwise hume false output milenga.
 
 console.log(studentName.includes("Suraj Raj"))
 // output:false 
@@ -105,10 +104,14 @@ output: [ 'thor', 'ironman', 'spiderman', [ 'Siperman', 'Flash', 'batman' ] ];
 // is array mein total 4 element hai : 1.thor 2.ironman 3.spiderman 4.['spiderman','Flash','batman'];
 
 console.log( typeof Marvel_heroes[0]);
-console.log(Dc_heroes[0][1]);
+// output:string 
+// output string ayenga array nhi kyuki hum marvel_heroes ke 0 index ki typeof nikal rahe hai to 0 index per humara thor aata hai or thor ek string hai array nhi
 
-const all = Marvel_heroes.push(Dc_heroes);
-console.log(all);
+console.log(Dc_heroes[0][1]);
+// output:i
+// output isme hum Dc_heroes ke 0 indexing ke 1 letter ko print kara rahe hai to humara 0 indexing par ironman aata hai or lekin hum ironman ke first letter ko print kara rahe hai jo ki i hai
+
+
 
 
 
